8ec8516b854147e917b1f0539de3e349d00e7b6a
Stefano Mondino <puntoste@gmail.com>
2018-07-11 17:43:57 +0200

    fix skeleton

--- Murray/Sources/MurrayCore/Skeleton.swift
+++ Murray/Sources/MurrayCore/Skeleton.swift
@@ -22,19 +22,23 @@ public final class Skeleton {
             let fs = FileSystem()
             let folder = try fs.createFolder(at: projectName)
             FileManager.default.changeCurrentDirectoryPath(folder.path)
-            
+        
             let url = URL(string:"git@github.com:synesthesia-it/Murray.git")!
             print ("Cloning Murray from \(url.absoluteString)")
             try shellOut(to:.gitClone(url:url))
             print ("Reorganizing folders")
             let murrayFolder = try folder.subfolder(named: "Murray")
+            print ("Removing useless folders from cloned template")
             try murrayFolder.subfolder(named: ".git").delete()
+            try murrayFolder.subfolder(named: "Murray").delete()
+            print ("Moving contents to proper folder")
             try murrayFolder.moveContents(to: folder, includeHidden: true)
             print ("Environment setup")
-            
             try shellOut(to: ShellOutCommand(string: "./install.sh"),
                          outputHandle:FileHandle.standardOutput,
                          errorHandle:FileHandle.standardError)
+            print ("Done!")
+            exit(0)
             
         } catch {
             throw Error.existingFolder
@@ -45,5 +49,6 @@ public extension Skeleton {
     enum Error: Swift.Error {
         case missingProjectName
         case existingFolder
+        case gitError
     }
 }
