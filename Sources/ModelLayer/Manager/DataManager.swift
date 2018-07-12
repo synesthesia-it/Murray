54673de5511de3e9035ec67223e4755b97c7a667
Stefano Mondino <puntoste@gmail.com>
2018-07-12 14:00:28 +0200

    refactor

--- Sources/ModelLayer/Manager/DataManager.swift
+++
@@ -1,7 +0,0 @@
-import Moya
-import RxSwift
-import Gloss
-
-class DataManager : ReactiveCompatible {
-    static let provider = MoyaProvider<API>(plugins: [NetworkLoggerPlugin(verbose: true, cURL: true)])
-}
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bundle install --path vendor/bundle
-bundle exec pod install --repo-update
-open *space;
-exit 0
