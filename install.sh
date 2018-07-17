9965179a090a6a394c350693379f76fa0e6ccffa
Stefano Mondino <puntoste@gmail.com>
2018-07-17 15:27:37 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+#
+REPONAME="Murray"
+
+function installTemplates {
+
+	cd $REPONAME
+	swift build -c release -Xswiftc -static-stdlib
+	cp -f .build/release/Murray /usr/local/bin/murray
+	cd ..
+	rm -rf $REPONAME
+	echo "Boomerang templates installed"
+}
+
+cd /tmp/
+rm -rf $REPONAME
+git clone https://github.com/synesthesia-it/Murray.git && installTemplates
