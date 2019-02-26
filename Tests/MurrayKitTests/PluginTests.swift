cc23b1828ad75f01a1803e2e98068878e88eb412
Stefano Mondino <>
2019-02-26 14:38:46 +0100

    tests

--- Tests/MurrayKitTests/PluginTests.swift
+++ Tests/MurrayKitTests/PluginTests.swift
@@ -19,6 +19,9 @@ class PluginsSpec: QuickSpec {
         
         it("test") {
             expect { try PluginManager.initializeBones(context: BonePluginContext()) }.notTo(throwError())
+            expect { try PluginManager.finalizeBones(context: BonePluginContext()) }.notTo(throwError())
+            expect { try PluginManager.afterReplace(context: BonePluginContext(), file: File(path: "/tmp/tmp.txt")) }.notTo(throwError())
+            
 //            PluginManager.bones()
 //                .forEach { plugin in
 //                    try plugin.finalize(context: BonePluginContext(boneSpec: nil, currentBone: nil, context: [:]))
