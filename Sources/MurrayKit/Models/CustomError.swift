3568c8bda03b2c0e26283d4d12e3d59300c755a7
Stefano Mondino <stefano.mondino.dev@gmail.com>
2020-02-26 09:22:18 +0100

    error description

--- Sources/MurrayKit/Models/CustomError.swift
+++ Sources/MurrayKit/Models/CustomError.swift
@@ -29,6 +29,24 @@ public enum CustomError: Swift.Error {
     
     case invalidJSONString
     case generic
-    
-    
+
+}
+
+extension CustomError: CustomStringConvertible {
+    public var description: String {
+
+        switch self {
+        case .undecodable(let file, let type): return "JSON file at \(file.path) is invalid and not convertible in \(type) entity"
+        case .fileNotFound(let path, let folder): return "File at \(path) in folder \(folder?.path ?? "-") not found."
+        case .unableToCreateFile(let path, let folder, _): return "Unable to create file at \(path) in folder \(folder?.path ?? "")."
+        case .unableToCreateFolder(let path, let folder): return "Unable to create folder \(path) in folder \(folder?.path ?? "")"
+        case .invalidPath(let path): return "Invalid path at \(path)"
+        case .unresolvableString: return "Provided string is not resolvable."
+        case .boneGroupNotFound(let name, _): return "Group named \(name) not found."
+        case .invalidMurrayfile: return "Provided Murrayfile.json is invalid."
+        case .missingRequiredParameter(let bone, let parameter): return "Missing\(parameter.name) parameter for item named \(bone.name)."
+        case .invalidJSONString: return "Invalid JSON string"
+        case .generic: return "Some error occurred. Please try again later."
+        }
+    }
 }
