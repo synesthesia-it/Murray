d04a7974ad45b3aab5d8ef0edc66dd959a1544b5
Stefano Mondino <stefano.mondino.dev@gmail.com>
2021-10-13 16:34:42 +0200

    fix: compilation issue

--- Tests/MurrayKitTests/Pipeline/SkeletonPipelineTests.swift
+++ Tests/MurrayKitTests/Pipeline/SkeletonPipelineTests.swift
@@ -24,14 +24,12 @@ class SkeletonPipelineSpec: QuickSpec {
                     try! Mocks.Scenario.skeleton(from: skeleton)
                 }
                 it("should properly create data in target folder") {
-                    expect {
-                        let pipeline = try SkeletonPipeline(folder: root, projectName: "Murray")
+                    let pipeline = try SkeletonPipeline(folder: root, projectName: "Murray")
 
-                        expect { try pipeline.execute(projectPath: skeleton.path, with: [:]) }.notTo(throwError())
-                        expect { try root.subfolder(at: "Murray/Murray.xcodeproj") }.notTo(throwError())
-                        expect { try root.file(at: "Murray/Murray/Murray.swift") }.notTo(throwError())
-                        expect { try root.file(at: "Murray/TouchMurray.txt") }.notTo(throwError())
-                    }
+                    expect { try pipeline.execute(projectPath: skeleton.path, with: [:]) }.notTo(throwError())
+                    expect { try root.subfolder(at: "Murray/Murray.xcodeproj") }.notTo(throwError())
+                    expect { try root.file(at: "Murray/Murray/Murray.swift") }.notTo(throwError())
+                    expect { try root.file(at: "Murray/TouchMurray.txt") }.notTo(throwError())
                 }
             }
         }
